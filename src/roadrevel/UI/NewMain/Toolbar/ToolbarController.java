cf1f550687a68f08bcb59b2f5d470beb77408f65
Mayahi Nasr <mayahinasr@gmail.com>
2023-03-09 03:13:50 +0100

    1st commit

--- src/roadrevel/UI/NewMain/Toolbar/ToolbarController.java
+++ src/roadrevel/UI/NewMain/Toolbar/ToolbarController.java
@@ -27,9 +27,7 @@ public class ToolbarController implements Initializable {
     @FXML
     private void loadConverter(ActionEvent event) {
                                         System.out.println(" Convert Money ");
-        Util.loadWindow(getClass().getResource("/roadrevel/UI/Converter/Converter.fxml"), "Add New Place", null);
+        Util.loadWindow(getClass().getResource("/roadrevel/UI/Converter/Converter.fxml"), "Convert Money", null);
     }
-
-
 
 }
